5cfae7ddee3adb75d911f776c3199c5f943687c6
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-09-16 22:01:47 -0300

    chore: update template

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,17 @@
-export default {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
+  // testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)': '<rootDir>/src/$1'
+  },
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
 }
+
+export default config
